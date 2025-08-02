aed79b49a3c90407e914e696d68344907ced2842
aryan-developer1 <aryandeveloper41@gmail.com>
2025-08-03 01:19:36 +0530

    changed url

--- Backend/index.js
+++ Backend/index.js
@@ -1,22 +1,34 @@
-const express = require('express')
-const connectDb = require("./db/db_connection")
-const diaryRoutes = require("./routes/diary.routes")
-const cors = require("cors")
-const app = express()
-const port = 3000
+const express = require("express");
+const connectDb = require("./db/db_connection");
+const diaryRoutes = require("./routes/diary.routes");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://digital-diary-ixpxbkv32-aryan-sonis-projects-8bf9baae.vercel.app",
+];
 
-app.use(cors({
-    origin:"http://localhost:5173"
-}));
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true); // ✅ Allow
+      } else {
+        callback(new Error("Not allowed by CORS")); // ❌ Block
+      }
+    },
+  })
+);
 app.use(express.json());
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.use("/api/v1/diary",diaryRoutes)
+app.use("/api/v1/diary", diaryRoutes);
 
-connectDb()
+connectDb();
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
